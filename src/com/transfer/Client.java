17ee68e1e8baffba4fd3dbb28161770e0e9cd8d3
mazi1 <matjazpogacnik5@gmail.com>
2021-02-22 15:45:50 +0100

    Scan directory and send files

--- src/com/transfer/Client.java
+++ src/com/transfer/Client.java
@@ -2,24 +2,34 @@ package com.transfer;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Client {
+    private static final String sendPath= "/home/matjaz/Programming/File-transfer/Data/";
+
     private static DataOutputStream dataOutputStream = null;
     private static DataInputStream dataInputStream = null;
+    private static ArrayList<File> filesArr = new ArrayList<File>();
 
     public static void main(String[] args) {
         try(Socket socket = new Socket("localhost",5000)) {
             dataInputStream = new DataInputStream(socket.getInputStream());
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-            dataInputStream
+            dataOutputStream.flush();
 
-            sendFile("/home/matjaz/Programming/File-transfer/Data/Copy");
-            sendFile("/home/matjaz/Programming/File-transfer/Data/Document");
-            sendFile("/home/matjaz/Programming/File-transfer/Data/rrr");
+            listFiles(sendPath); //scan directory and add non directory files to ArrayList files
+            dataOutputStream.writeInt(filesArr.size()); //send number of sent files
+
+            for(int i = 0 ; i < filesArr.size();i++){
+                dataOutputStream.writeUTF(filesArr.get(i).getName()); //send names of sent files
+                dataOutputStream.flush();
+
+                sendFile(filesArr.get(i).getAbsolutePath());
+            }
 
             dataInputStream.close();
-            dataInputStream.close();
+            dataOutputStream.close();
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -30,14 +40,28 @@ public class Client {
         File file = new File(path);
         FileInputStream fileInputStream = new FileInputStream(file);
 
-        // send file size
-        dataOutputStream.writeLong(file.length());
-        // break file into chunks
-        byte[] buffer = new byte[4*1024];
+        dataOutputStream.writeLong(file.length()); //send file size
+
+        byte[] buffer = new byte[4*1024]; //buffer for file pieces
         while ((bytes=fileInputStream.read(buffer))!=-1){
             dataOutputStream.write(buffer,0,bytes);
             dataOutputStream.flush();
         }
         fileInputStream.close();
+    }
+
+    public static void listFiles(String startDir) {
+        File dir = new File(startDir);
+        File[] files = dir.listFiles();
+
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listFiles(file.getAbsolutePath());
+                } else {
+                    filesArr.add(file);
+                }
+            }
+        }
     }
 }
--- src/com/transfer/Server.java
+++ src/com/transfer/Server.java
@@ -7,24 +7,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private static final String receivePath = "/home/matjaz/Programming/File-transfer/Received/";
+
     private static DataOutputStream dataOutputStream = null;
     private static DataInputStream dataInputStream = null;
 
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(5000)){
-            System.out.println("listening to port:5000");
+        try(ServerSocket serverSocket = new ServerSocket(5000)){ //listening to port:5000
             Socket clientSocket = serverSocket.accept();
-            System.out.println(clientSocket+" connected.");
+
             dataInputStream = new DataInputStream(clientSocket.getInputStream());
             dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 
-            receiveFile("/home/matjaz/Programming/File-transfer/Received/Copy");
-            receiveFile("/home/matjaz/Programming/File-transfer/Received/Document");
-            receiveFile("/home/matjaz/Programming/File-transfer/Received/rrr");
+            int number = dataInputStream.readInt(); //number of files to be received
+
+            for(int i = 0; i < number; i++) { //for number of files to be received get file name and append to receivePath
+                receiveFile(receivePath + dataInputStream.readUTF());
+            }
 
             dataInputStream.close();
             dataOutputStream.close();
-            clientSocket.close();
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -32,13 +34,13 @@ public class Server {
 
     private static void receiveFile(String fileName) throws Exception{
         int bytes = 0;
-        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName); //output to file
 
-        long size = dataInputStream.readLong();     // read file size
+        long size = dataInputStream.readLong(); //get size of file
         byte[] buffer = new byte[4*1024];
         while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
             fileOutputStream.write(buffer,0,bytes);
-            size -= bytes;      // read upto file size
+            size -= bytes; //decrease size of file by received bytes
         }
         fileOutputStream.close();
     }
